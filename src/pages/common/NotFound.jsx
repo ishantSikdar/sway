a7730e9ac343cd433df7c5c79d51d4da749a5487
ishantSikdar <ishant9715@gmail.com>
2024-04-02 12:09:03 +0530

    Update NotFound Page, displayed Route

--- src/pages/common/NotFound.jsx
+++ src/pages/common/NotFound.jsx
@@ -1,13 +1,21 @@
-const NotFound = () => {
+import { useLocation } from "react-router";
+
+export default function NotFound () {
+
+    const location = useLocation();
+    const { pathname } = location;
+
     return (
         <div className="relative flex flex-col items-center justify-center h-screen bg-gray-100">
             <div className="absolute top-[30%] max-w-lg text-center px-4">
-                <h1 className="text-4xl font-semibold text-gray-800 mb-4">Page Not Found</h1>
-                <p className="text-lg text-gray-600 mb-4">Oops! The page you are looking for does not exist.</p>
-                <p className="text-gray-600 mb-8">Please check the URL or go back to the <a href="/" className="text-blue-600 hover:underline">homepage</a>.</p>
+                <h1 className="text-4xl font-semibold text-gray-800 mb-4">
+                    Page Not Found
+                </h1>
+                <p className="text-gray-600">
+                    The requested URL <strong>{pathname}</strong> was not found on this server.
+                </p>
+                <p className="text-gray-600">Please check the URL or go back to the <a href="/" className="text-blue-600 hover:underline">homepage</a>.</p>
             </div>
         </div>
     );
 };
-
-export default NotFound;
